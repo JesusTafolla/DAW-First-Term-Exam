eaff44aecf0496fd0a129610c8e50c6c0feb751e
Luis Enrique <luisemezag@gmail.com>
2021-09-12 19:27:59 -0500

    Finished API and Index for issue #3 and #4

--- api_methods.js
+++ api_methods.js
@@ -17,7 +17,7 @@ function addComment() {
     objForm.names = nameElmnt.value;
     objForm.country=countryElmnt.value;
     objForm.comment = commentElmnt.value
-    
+
     console.log("INFORMATION:");
     console.log(objForm);
 
@@ -25,5 +25,55 @@ function addComment() {
     console.log(formJSON);
 
     var jsonElement = document.getElementById("myjson");
-    jsonElement.innerHTML = "Comments and information sent succesfully!!!"; 
+    jsonElement.innerHTML = "Comments and information sent succesfully!!!";
+}
+
+function getSpotifySong(){
+    var results = document.getElementById("spotify-result");
+
+    var keyword = document.getElementById("keyword");
+    var token = document.getElementById("token");
+
+    var request = new Request(
+      `https://api.spotify.com/v1/search?q=${keyword.value}&type=track%2Cartist&market=US&limit=3&offset=1`,
+      {
+        method: 'GET',
+        headers: new Headers({
+          'Content-type': 'application/json',
+          'Authorization': 'Bearer ' + token.value
+        })
+      }
+    );
+
+    fetch(request)
+    .then(function(data){
+      console.log('data');
+      console.log(data);
+      data.json()
+      .then(function(myjson){
+        console.log('results = ');
+        console.log(myjson);
+
+        document.getElementById("artist-1-name").innerHTML = myjson.artists.items[0].name;
+        document.getElementById("artist-2-name").innerHTML = myjson.artists.items[1].name;
+        document.getElementById("artist-3-name").innerHTML = myjson.artists.items[2].name;
+
+        document.getElementById("track-1-name").innerHTML = myjson.tracks.items[0].name;
+        document.getElementById("track-2-name").innerHTML = myjson.tracks.items[1].name;
+        document.getElementById("track-3-name").innerHTML = myjson.tracks.items[2].name;
+
+        document.getElementById("artist-1").src = myjson.artists.items[0].images[0].url;
+        document.getElementById("artist-2").src = myjson.artists.items[1].images[0].url;
+        document.getElementById("artist-3").src = myjson.artists.items[2].images[0].url;
+
+        document.getElementById("track-1").src = myjson.tracks.items[0].album.images[0].url;
+        document.getElementById("track-2").src = myjson.tracks.items[1].album.images[0].url;
+        document.getElementById("track-3").src = myjson.tracks.items[2].album.images[0].url;
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+    results.style.display = "block";
 }
